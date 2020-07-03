210ee3d82b5177f0d871cfaebe28229aedd12413
Carlos Fabián <carl.fab.g@gmail.com>
2020-07-03 01:00:33 -0500

    estilos de navbar mobile

---
+++ main.js
@@ -0,0 +1,10 @@
+const $navMenu = document.querySelector('.navbar-mobile')
+const $burgerButton = document.querySelector('#burger-button')
+const $body = document.querySelector('body')
+
+
+$burgerButton.addEventListener('click', () => {
+    $navMenu.classList.toggle('active')
+    $body.classList.toggle('scroll')
+    $burgerButton.classList.toggle('active')
+})
